6a895e0a6cb1f5129379cb5405821862be97a49e
Wojciech Kaluza <wkaluza@protonmail.com>
2021-07-30 21:36:00 +0200

    Add GitHub repo creation script

---
+++ new_github_repository.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function main() {
+  local repo_name="$1"
+  local repo_description="$2"
+
+  local repo_url="git@github.com:wkaluza/${repo_name}"
+
+  gh api user/repos \
+    --method POST \
+    --header "Accept: application/vnd.github.v3+json" \
+    --field private=true \
+    --field has_issues=false \
+    --field has_projects=false \
+    --field has_wiki=false \
+    --field allow_squash_merge=true \
+    --field allow_merge_commit=false \
+    --field allow_rebase_merge=false \
+    --field delete_branch_on_merge=true \
+    --field name="${repo_name}" \
+    --field description="${repo_description}"
+
+  git init "${repo_name}"
+  cd "${repo_name}"
+  git remote add origin "${repo_url}"
+  git fetch --all --recurse-submodules --tags
+  git commit --allow-empty --message "Repository root"
+  git push --set-upstream origin main
+}
+
+# Entry point
+main "$1" "$2"
