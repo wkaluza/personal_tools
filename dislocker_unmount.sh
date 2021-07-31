96369cd4bb3213e7df8ced544ad9262b0c0bc222
Wojciech Kaluza <wkaluza@protonmail.com>
2021-07-31 10:06:45 +0200

    Add dislocker helper scripts

---
+++ dislocker_unmount.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function main() {
+  local dislocker_dir="$1"
+  local mount_point="$2"
+
+  sudo umount -d "${mount_point}"
+  sleep 5
+  sudo umount -d "${dislocker_dir}"
+}
+
+# Entry point
+main "$1" "$2"
