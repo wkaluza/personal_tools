02ac6a3f4791579b2456a880dfbd463e099b4ab0
Wojciech Kaluza <wkaluza@protonmail.com>
2021-08-01 19:57:55 +0200

    Disable NPM post-install scripts for security

--- installer_scripts/set_up_os_ubuntu.sh
+++ installer_scripts/set_up_os_ubuntu.sh
@@ -180,6 +180,8 @@ function install_nodejs() {
 
   curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
   sudo apt-get install -y nodejs >/dev/null
+
+  npm config set ignore-scripts true
 }
 
 function configure_bash() {
