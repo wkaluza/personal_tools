5c214ba0769780f943f83f51674d6d85ded9718e
Wojciech Kaluza <wkaluza@protonmail.com>
2021-08-02 12:45:08 +0200

    Add script for publishing PGP keys

---
+++ pgp_key_generation/send_pgp_keys_to_keyserver.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function main() {
+  local primary_key_fingerprint="$1"
+
+  gpg --keyserver keys.openpgp.org --send-keys "${primary_key_fingerprint}"
+  gpg --keyserver pgp.mit.edu --send-keys "${primary_key_fingerprint}"
+  gpg --keyserver keyserver.ubuntu.com --send-keys "${primary_key_fingerprint}"
+}
+
+# Entry point
+main "$1"
