cdff8b32eef9119cb7ec2d1a3e3d39c01a25f08a
Wojciech Kaluza <wkaluza@protonmail.com>
2021-08-17 21:46:50 +0200

    Delete running containers after prune

--- clean_docker_cache.sh
+++ clean_docker_cache.sh
@@ -6,16 +6,27 @@ source "${THIS_SCRIPT_DIR}/shell_script_imports/logging.sh"
 
 function main() {
   local exited_containers
-  exited_containers=$(docker container list --all --quiet --filter status=exited)
+  exited_containers=$(docker container list --all --quiet --filter="status=exited")
   if test -z "$exited_containers"; then
     log_info "No exited containers"
   else
     log_info "Deleting exited containers"
-    docker container rm --volumes $exited_containers
+    docker rm --volumes $exited_containers
   fi
 
   log_info "Pruning unused images"
   docker image prune --all --force
+
+  local running_containers
+  running_containers=$(docker container list --all --quiet --filter="status=running")
+  if test -z "$running_containers"; then
+    log_info "No running containers"
+  else
+    log_info "Stopping running containers"
+    docker stop $running_containers
+    log_info "Deleting..."
+    docker rm --volumes $running_containers
+  fi
 }
 
 # Entry point
