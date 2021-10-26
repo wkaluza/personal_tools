dbf51f1eadda23cdc53b3cd4cbb7fa6dd0691544
Wojciech Kaluza <wkaluza@protonmail.com>
2021-10-26 11:52:25 +0200

    Remove explicit dislocker password

--- dislocker_mount.sh
+++ dislocker_mount.sh
@@ -6,15 +6,14 @@ function main() {
   local encrypted_partition="$1"
   local dislocker_dir="$2"
   local mount_point="$3"
-  local password="$4"
 
   sudo mkdir -p "${dislocker_dir}"
   sudo mkdir -p "${mount_point}"
 
-  sudo dislocker "${encrypted_partition}" -u${password} -- "${dislocker_dir}"
+  sudo dislocker "${encrypted_partition}" -u -- "${dislocker_dir}"
   sleep 5
   sudo mount -o loop "${dislocker_dir}/dislocker-file" "${mount_point}"
 }
 
 # Entry point
-main "$1" "$2" "$3" "$4"
+main "$1" "$2" "$3"
