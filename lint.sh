188a1f64594826b2b39e14bd5a9d49661e694f20
Wojciech Kaluza <wkaluza@protonmail.com>
2021-11-08 10:52:55 +0100

    Add formatting script

---
+++ lint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function main {
+  find . -type f -iname '*.sh' -or -iname '*.bash' |
+    xargs --no-run-if-empty --max-lines=1 shfmt -i 2 -w
+}
+
+# Entry point
+main
