13bf1f851bf0992ca90040d03dc65d913f386e69
Wojciech Kaluza <wkaluza@protonmail.com>
2021-12-09 20:00:36 +0100

    Clean up script
    * Add missing tracing
    * Move conditional up the call stack
    * Ensure noninteractive apt-get
    * Handle case when $USER is not defined

--- installer_scripts/install_docker.bash
+++ installer_scripts/install_docker.bash
@@ -8,50 +8,66 @@ source "${THIS_SCRIPT_DIR}/../shell_script_imports/logging.bash"
 source "${THIS_SCRIPT_DIR}/../shell_script_imports/common.bash"
 
 function install_docker {
-  if ! docker --version >/dev/null; then
-    log_info "Installing docker"
+  print_trace
 
-    local url="https://download.docker.com/linux/ubuntu"
-    local key="/usr/share/keyrings/docker-archive-keyring.gpg"
+  local url="https://download.docker.com/linux/ubuntu"
+  local key="/usr/share/keyrings/docker-archive-keyring.gpg"
 
-    sudo apt-get update >/dev/null
-    sudo apt-get install -y \
-      apt-transport-https \
-      ca-certificates \
-      gnupg \
-      lsb-release \
-      curl >/dev/null
+  sudo apt-get update >/dev/null
+  DEBIAN_FRONTEND=noninteractive sudo \
+    --preserve-env=DEBIAN_FRONTEND apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    gnupg \
+    lsb-release \
+    curl >/dev/null
 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg |
-      sudo gpg --dearmor -o "${key}"
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg |
+    sudo gpg --dearmor -o "${key}"
 
-    echo \
-      "deb [arch=amd64 signed-by=${key}] ${url} $(lsb_release -cs) stable" |
-      sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  echo \
+    "deb [arch=amd64 signed-by=${key}] ${url} $(lsb_release -cs) stable" |
+    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-    sudo apt-get update >/dev/null
-    sudo apt-get install -y \
-      docker-ce \
-      docker-ce-cli \
-      containerd.io >/dev/null
+  sudo apt-get update >/dev/null
+  DEBIAN_FRONTEND=noninteractive sudo \
+    --preserve-env=DEBIAN_FRONTEND apt-get install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io >/dev/null
 
-    sudo systemctl enable docker.service
-    sudo systemctl enable containerd.service
+  sudo systemctl enable docker.service
+  sudo systemctl enable containerd.service
 
-    # Create the docker group if it does not exist
-    sudo getent group docker || sudo groupadd docker
-    sudo usermod -aG docker "$USER" >/dev/null
+  # Create the docker group if it does not exist
+  sudo getent group docker || sudo groupadd docker
+  if [[ "${USER:-_user_not_defined_}" == "_user_not_defined_" ]]; then
+    log_warning "Current user not defined"
   else
+    sudo usermod -aG docker "$USER" >/dev/null
+  fi
+}
+
+function install_docker_unless_already_installed {
+  print_trace
+
+  if docker --version >/dev/null 2>&1; then
     log_info "docker is already installed"
     docker run --rm hello-world >/dev/null
     docker info
+  else
+    log_info "Installing docker"
+    install_docker
+    log_info "Success! Reboot required."
+    log_info "Next: configure credential provider"
   fi
 }
 
 function main {
   ensure_not_sudo
-  install_docker
-  log_info "Success! Reboot required."
+  install_docker_unless_already_installed
+
+  echo Success
 }
 
 # Entry point
