49f65bfd0307c4a3cffb53555bc76d8d1080c641
Wojciech Kaluza <wkaluza@protonmail.com>
2021-12-19 17:06:45 +0100

    Clean up docker reclamation script

--- clean_docker_cache.bash
+++ clean_docker_cache.bash
@@ -5,15 +5,6 @@ THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 source "${THIS_SCRIPT_DIR}/shell_script_imports/logging.bash"
 
 function main {
-  local exited_containers
-  exited_containers=$(docker container list --all --quiet --filter="status=exited")
-  if test -z "$exited_containers"; then
-    log_info "No exited containers"
-  else
-    log_info "Deleting exited containers"
-    docker rm --volumes $exited_containers
-  fi
-
   local running_containers
   running_containers=$(docker container list --all --quiet --filter="status=running")
   if test -z "$running_containers"; then
@@ -21,12 +12,19 @@ function main {
   else
     log_info "Stopping running containers"
     docker stop $running_containers
-    log_info "Deleting..."
-    docker rm --volumes $running_containers
   fi
 
-  # log_info "Pruning unused images"
-  # docker image prune --all --force
+  local exited_containers
+  exited_containers=$(docker container list --all --quiet --filter="status=exited")
+  if test -z "$exited_containers"; then
+    log_info "No exited containers"
+  else
+    log_info "Deleting exited containers"
+    docker rm $exited_containers
+  fi
+
+  log_info "Pruning unused images"
+  docker image prune --force
 }
 
 # Entry point
