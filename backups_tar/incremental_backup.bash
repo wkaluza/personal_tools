88eaedb694b2e55f019c95125bd82bfca3e1b7f8
Wojciech Kaluza <wkaluza@protonmail.com>
2022-01-12 20:52:40 +0100

    Create tar backup automation scripts

---
+++ backups_tar/incremental_backup.bash
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+TEMP_UNPACK_DIR="$HOME/not_a_real_directory"
+
+function on_exit
+{
+  rm -rf "${TEMP_UNPACK_DIR}"
+}
+
+trap on_exit EXIT
+
+function main
+{
+  local dir_to_back_up
+  dir_to_back_up="$(realpath "$1")"
+  local backup_dir
+  backup_dir="$(realpath "$2")"
+
+  if ! test -d "${dir_to_back_up}"; then
+    echo "${dir_to_back_up} is not a directory"
+    exit 1
+  fi
+
+  mkdir --parents "${backup_dir}"
+
+  local now
+  now="$(date --utc +'%Y%m%d%H%M%S%N')"
+
+  local snapshot_file
+  snapshot_file="$(find "${backup_dir}" -type f -name 'tar_snapshot_*')"
+  if test -f "${snapshot_file}"; then
+    snapshot_file="$(realpath "${snapshot_file}")"
+  else
+    snapshot_file="${backup_dir}/tar_snapshot_${now}"
+  fi
+
+  tar \
+    --directory "$(dirname "${dir_to_back_up}")" \
+    --listed-incremental="${snapshot_file}" \
+    --create \
+    --gzip \
+    --file "${backup_dir}/backup_${now}.tar.gz" \
+    "./$(basename "${dir_to_back_up}")"
+
+  TEMP_UNPACK_DIR="$(dirname "${backup_dir}")/temp_unpack_$(basename "${backup_dir}")"
+  mkdir --parents "${TEMP_UNPACK_DIR}"
+
+  echo "Performing test restoration..."
+
+  for f in $(find "${backup_dir}" -type f -name 'backup_*.tar.gz' | sort); do
+    echo "  Extracting $(realpath "${f}")"
+
+    tar \
+      --directory "${TEMP_UNPACK_DIR}" \
+      --listed-incremental=/dev/null \
+      --extract \
+      --gzip \
+      --file "$(realpath "${f}")"
+  done
+
+  echo "Test restoration done"
+  echo "Performing comparison..."
+
+  if ! diff --recursive "${TEMP_UNPACK_DIR}/$(basename "${dir_to_back_up}")" "${dir_to_back_up}"; then
+    echo "Test recovery failed: diff did not match with original"
+    rm "${backup_dir}/backup_${now}.tar.gz"
+
+    exit 1
+  fi
+
+  echo Success
+}
+
+# Entry point
+main "$1" "$2"
---
+++ backups_tar/restore_backup.bash
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function main
+{
+  local restore_destination
+  restore_destination="$(realpath "$1")"
+  local backup_dir
+  backup_dir="$(realpath "$2")"
+
+  mkdir --parents "${restore_destination}"
+
+  if ! test -z "$(ls -A "${restore_destination}")"; then
+    echo "${restore_destination} is not empty"
+    exit 1
+  fi
+
+  mkdir --parents "${backup_dir}"
+
+  echo "Performing restoration..."
+
+  for f in $(find "${backup_dir}" -type f -name 'backup_*.tar.gz' | sort); do
+    echo "  Extracting $(realpath "${f}")"
+
+    tar \
+      --directory "${restore_destination}" \
+      --listed-incremental=/dev/null \
+      --extract \
+      --gzip \
+      --file "$(realpath "${f}")"
+  done
+
+  echo Success
+}
+
+# Entry point
+main "$1" "$2"
