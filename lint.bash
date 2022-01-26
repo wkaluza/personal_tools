a629a5dc94ca9a4c2b57e626be2ac29a12d268b6
Wojciech Kaluza <wkaluza@protonmail.com>
2022-01-26 17:53:09 +0100

    Lint files in parallel

--- lint.bash
+++ lint.bash
@@ -7,18 +7,26 @@ THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 function main
 {
   for f in $(find "${THIS_SCRIPT_DIR}" -type f -iname '*.bash'); do
-    shfmt -i 2 -fn -w "${f}"
+    echo "${f}"
+    shfmt -i 2 -fn -w "${f}" &
   done
 
   for f in $(find "${THIS_SCRIPT_DIR}" -type f -iname '*.json'); do
+    echo "${f}"
+
     local json_text
     json_text="$(cat "${f}")"
 
     echo "${json_text}" |
       jq --sort-keys \
         '.' \
-        - >"${f}"
+        - >"${f}" &
   done
+
+  echo "Waiting..."
+  wait
+
+  echo "Success"
 }
 
 # Entry point
