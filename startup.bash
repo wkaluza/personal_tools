a16a5fd463be161628ca81005286c612066efdbd
Wojciech Kaluza <wkaluza@protonmail.com>
2022-02-28 21:20:06 +0100

    Move LUKS scripts to subdirectory

--- startup.bash
+++ startup.bash
@@ -4,7 +4,7 @@ THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 function main
 {
-  $THIS_SCRIPT_DIR/luks_mount.bash sda
+  $THIS_SCRIPT_DIR/luks/mount.bash sda
   $THIS_SCRIPT_DIR/upgrade_apt_full.bash
 }
 
