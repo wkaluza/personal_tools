ef57cdc08d9815f513c659e0130ac335fb7ab769
Wojciech Kaluza <wkaluza@protonmail.com>
2022-04-05 22:34:39 +0200

    Dockerise bare minimum script

---
+++ installer_scripts/test_ubuntu_bare_minimum.dockerfile
@@ -0,0 +1,49 @@
+FROM ubuntu:22.04
+
+ARG USER_ID
+ARG GROUP_ID
+ARG USERNAME
+ARG TIMEZONE
+ARG DOCKER_GROUP_ID
+
+ENV USER=$USERNAME
+ENV UID=$USER_ID
+ENV HOST_TIMEZONE=$TIMEZONE
+
+ARG _HOME=/home/$USERNAME
+ARG _WORKSPACE=$_HOME/workspace
+
+SHELL ["bash","-c"]
+
+RUN apt-get update
+RUN apt-get install --yes sudo
+
+RUN addgroup \
+--gid "$GROUP_ID" \
+"$USERNAME"
+
+RUN adduser \
+--disabled-password \
+--gecos "" \
+--gid "$GROUP_ID" \
+--home "$(realpath $_HOME)" \
+--shell "/bin/bash" \
+--uid "$USER_ID" \
+"$USERNAME"
+
+RUN mkdir --parents $_WORKSPACE
+COPY ubuntu_bare_minimum.bash $_WORKSPACE/
+RUN chown --recursive $USERNAME:$USERNAME $_HOME
+
+RUN adduser "$USERNAME" sudo
+RUN echo "%sudo ALL=(ALL) NOPASSWD:ALL" >>"/etc/sudoers"
+
+RUN addgroup \
+--gid "$DOCKER_GROUP_ID" \
+docker
+
+RUN adduser "$USERNAME" docker
+
+USER $USERNAME
+
+WORKDIR $_WORKSPACE
---
+++ installer_scripts/test_ubuntu_bare_minimum_in_docker.bash
@@ -0,0 +1,44 @@
+set -euo pipefail
+
+THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+DOCKER_IMAGE_TAG="test_ubuntu_bare_minimum:latest"
+
+function on_exit
+{
+  docker image rm \
+    --force \
+    --no-prune \
+    "${DOCKER_IMAGE_TAG}"
+}
+
+trap on_exit EXIT
+
+source "${THIS_SCRIPT_DIR}/../shell_script_imports/logging.bash"
+
+function main
+{
+  local docker_socket="/var/run/docker.sock"
+
+  docker build \
+    --build-arg "USER_ID=$(id -u)" \
+    --build-arg "GROUP_ID=$(id -g)" \
+    --build-arg "USERNAME=$(id -un)" \
+    --build-arg "TIMEZONE=$(readlink -e /etc/localtime)" \
+    --build-arg "WORKSPACE=$(pwd)" \
+    --build-arg "DOCKER_GROUP_ID=$(getent group docker | awk -F: '{print $3}')" \
+    --file "${THIS_SCRIPT_DIR}/test_ubuntu_bare_minimum.dockerfile" \
+    --tag "${DOCKER_IMAGE_TAG}" \
+    "${THIS_SCRIPT_DIR}"
+
+  docker run \
+    --interactive \
+    --rm \
+    --tty \
+    --volume "${docker_socket}:${docker_socket}" \
+    "${DOCKER_IMAGE_TAG}" \
+    bash "${THIS_SCRIPT_DIR}/ubuntu_bare_minimum.bash"
+
+  log_info "Success: $(basename $0)"
+}
+
+main
