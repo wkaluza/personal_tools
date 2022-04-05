096cc4c46af02c5ca7ec5b51f86473385068a6b1
Wojciech Kaluza <wkaluza@protonmail.com>
2022-04-05 10:59:40 +0200

    Remove guard around docker swarm startup

--- startup.bash
+++ startup.bash
@@ -5,13 +5,8 @@ THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 function main
 {
   bash "${THIS_SCRIPT_DIR}/luks/mount.bash" sda
+  bash "${THIS_SCRIPT_DIR}/prepare_docker_swarm.bash"
   bash "${THIS_SCRIPT_DIR}/upgrade_apt_full.bash"
-
-  if docker --version >/dev/null; then
-    bash "${THIS_SCRIPT_DIR}/prepare_docker_swarm.bash"
-  else
-    echo "docker is not available"
-  fi
 }
 
 main
