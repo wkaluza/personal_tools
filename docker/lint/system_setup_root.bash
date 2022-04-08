11a032a85ddd8412d29fc6ef8d2e44c956a6c75e
Wojciech Kaluza <wkaluza@protonmail.com>
2022-04-08 15:47:07 +0200

    Remove unused variables

--- docker/lint/system_setup_root.bash
+++ docker/lint/system_setup_root.bash
@@ -1,7 +1,5 @@
 set -euo pipefail
 
-THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-
 function main
 {
   source "${IMPORTS_DIR}/files_root/root_import.bash"
--- docker/lint/system_setup_user.bash
+++ docker/lint/system_setup_user.bash
@@ -1,7 +1,5 @@
 set -euo pipefail
 
-THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-
 function main
 {
   source "${IMPORTS_DIR}/files_user/user_import.bash"
