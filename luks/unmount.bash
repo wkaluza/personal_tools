ddb2cad2b3dd54077f917fedc893b8b9e1a14e48
Wojciech Kaluza <wkaluza@protonmail.com>
2022-04-08 15:43:38 +0200

    Remove unused variables

--- luks/unmount.bash
+++ luks/unmount.bash
@@ -4,10 +4,8 @@ function main
 {
   local device="$1"
 
-  local encrypted_device="/dev/${device}"
   local mapping_name="pcspec_${device}_luks"
 
-  local mapped_device="/dev/mapper/${mapping_name}"
   local mount_point="${HOME}/luks/${mapping_name}"
 
   sudo umount "${mount_point}"
