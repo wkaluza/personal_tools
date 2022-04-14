37ab0cdd99d0ed5ce5e1861f1fd4c65a97b9986a
Wojciech Kaluza <wkaluza@protonmail.com>
2022-04-14 10:28:15 +0200

    Manage luks keys with pass

--- luks/mount.bash
+++ luks/mount.bash
@@ -1,9 +1,12 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
+KEY_TYPE_NOT_DEFINED="___KEY_TYPE_NOT_DEFINED___"
+
 function main
 {
   local device="$1"
+  local key_type="$2"
 
   local encrypted_device="/dev/${device}"
   local mapping_name="pcspec_${device}_luks"
@@ -13,9 +16,23 @@ function main
 
   mkdir --parents "${mount_point}"
 
-  sudo cryptsetup open "${encrypted_device}" "${mapping_name}"
+  if [[ "${key_type}" == "${KEY_TYPE_NOT_DEFINED}" ]]; then
+    sudo cryptsetup open \
+      "${encrypted_device}" \
+      "${mapping_name}"
+  else
+    pass show "luks_passphrase_${key_type}" |
+      tr -d '\n' |
+      sudo cryptsetup open \
+        --key-file - \
+        "${encrypted_device}" \
+        "${mapping_name}"
+  fi
+
   sudo mount "${mapped_device}" "${mount_point}"
+
+  echo "Success $(basename "$0")"
 }
 
 # Entry point
-main "$1"
+main "$1" "${2:-"${KEY_TYPE_NOT_DEFINED}"}"
--- startup.bash
+++ startup.bash
@@ -5,7 +5,9 @@ THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 function main
 {
-  bash "${THIS_SCRIPT_DIR}/luks/mount.bash" sda
+  bash "${THIS_SCRIPT_DIR}/luks/mount.bash" \
+    "sda" \
+    "system"
   bash "${THIS_SCRIPT_DIR}/prepare_docker_swarm.bash"
   bash "${THIS_SCRIPT_DIR}/upgrade_apt_full.bash"
 }
