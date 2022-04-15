e715b31ad38098e03a10a7cde65f64473f4906ca
Wojciech Kaluza <wkaluza@protonmail.com>
2022-04-15 11:46:12 +0200

    Automate fetching latest tools at startup

--- shell_script_imports/git_helpers.bash
+++ shell_script_imports/git_helpers.bash
@@ -43,3 +43,54 @@ function clone_or_fetch
   git fetch --all --recurse-submodules --tags --force
   popd
 }
+
+function repo_is_clean
+{
+  local repo_state="clean"
+
+  git add . >/dev/null
+
+  if ! git diff --cached; then
+    repo_state="dirty"
+  fi
+
+  git reset --mixed "HEAD" >/dev/null
+
+  if [[ "${repo_state}" == "dirty" ]]; then
+    false
+  fi
+}
+
+function clean_repo
+{
+  git add . >/dev/null
+  git reset --hard "HEAD" >/dev/null
+  git clean -dffx >/dev/null
+}
+
+function git_get_latest
+{
+  local remote_name="${1:-"origin"}"
+  local branch_name="${2:-"main"}"
+
+  if repo_is_clean; then
+    git checkout \
+      --force \
+      "${branch_name}" >/dev/null
+    git fetch \
+      --all \
+      --force \
+      --prune \
+      --prune-tags \
+      --recurse-submodules \
+      --tags >/dev/null
+    git reset \
+      --hard \
+      "${remote_name}/${branch_name}" >/dev/null
+
+    clean_repo
+  else
+    echo "Repository is not clean, aborting"
+    exit 1
+  fi
+}
--- startup.bash
+++ startup.bash
@@ -3,8 +3,15 @@ shopt -s inherit_errexit
 
 THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
+source "${THIS_SCRIPT_DIR}/shell_script_imports/common.bash"
+source "${THIS_SCRIPT_DIR}/shell_script_imports/git_helpers.bash"
+
 function main
 {
+  run_in_context \
+    "${THIS_SCRIPT_DIR}" \
+    git_get_latest
+
   bash "${THIS_SCRIPT_DIR}/luks/mount.bash" \
     "sda" \
     "system"
