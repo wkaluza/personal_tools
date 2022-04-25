fbcb65eda5c3648a7e9605e558b23e2f298bfcdf
Wojciech Kaluza <wkaluza@protonmail.com>
2022-04-25 13:14:41 +0200

    Only update startup repo when online

--- startup.bash
+++ startup.bash
@@ -8,9 +8,11 @@ source "${THIS_SCRIPT_DIR}/shell_script_imports/git_helpers.bash"
 
 function main
 {
-  run_in_context \
-    "${THIS_SCRIPT_DIR}" \
-    git_get_latest >/dev/null 2>&1
+  if web_connection_working; then
+    run_in_context \
+      "${THIS_SCRIPT_DIR}" \
+      git_get_latest >/dev/null 2>&1
+  fi
 
   bash "${THIS_SCRIPT_DIR}/startup_impl.bash"
 }
