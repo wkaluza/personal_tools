f1f9c54c76de782748252b28d5af6b31aeb629b7
Wojciech Kaluza <wkaluza@protonmail.com>
2022-10-15 20:58:25 +0200

    Move docker jobs to subdirectory

--- docker/jobs/run_in_docker.bash
+++ docker/jobs/run_in_docker.bash
@@ -5,7 +5,7 @@ fi
 THIS_SCRIPT_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 cd "${THIS_SCRIPT_DIR}"
 
-source "${THIS_SCRIPT_DIR}/../shell_script_imports/common.bash"
+source "${THIS_SCRIPT_DIR}/../../shell_script_imports/common.bash"
 
 DIGESTS=""
 
--- scripts/lint_in_docker.bash
+++ scripts/lint_in_docker.bash
@@ -10,9 +10,9 @@ function main
   local project_root_dir
   project_root_dir="$(realpath "${THIS_SCRIPT_DIR}/..")"
 
-  bash "${project_root_dir}/docker/run_in_docker.bash" \
+  bash "${project_root_dir}/docker/jobs/run_in_docker.bash" \
     "${project_root_dir}" \
-    "${project_root_dir}/docker/docker_jobs.json" \
+    "${project_root_dir}/docker/jobs/docker_jobs.json" \
     "lint"
 
   echo "Success: $(basename "$0")"
