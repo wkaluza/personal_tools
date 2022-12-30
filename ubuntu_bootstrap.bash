48dbb18e9b9c28f1fb8d974196d9e1e4e8e043af
Wojciech Kaluza <wkaluza@protonmail.com>
2022-12-30 17:00:00 +0100

    Refactor OS bootstrap script

--- ubuntu_bootstrap.bash
+++ ubuntu_bootstrap.bash
@@ -5,21 +5,38 @@ fi
 THIS_SCRIPT_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 cd "${THIS_SCRIPT_DIR}"
 
+function download_latest_personal_tools
+{
+  local temp_dir="$1"
+  local url="$2"
+
+  local temp_file
+  temp_file="$(mktemp)"
+
+  curl \
+    -fsSL \
+    --output - \
+    "${url}" >"${temp_file}"
+
+  unzip \
+    "${temp_file}" \
+    -d "${temp_dir}" &>/dev/null
+}
+
 function main
 {
   local repo_name="personal_tools"
   local branch_name="main"
   local url="https://github.com/wkaluza/${repo_name}/archive/refs/heads/${branch_name}.zip"
 
-  local temp_file
-  temp_file="$(mktemp)"
   local temp_dir
   temp_dir="$(mktemp -d)"
 
   local repo_dir="${temp_dir}/${repo_name}-${branch_name}"
 
-  curl -fsSL --output - "${url}" >"${temp_file}"
-  unzip "${temp_file}" -d "${temp_dir}" >/dev/null
+  download_latest_personal_tools \
+    "${temp_dir}" \
+    "${url}"
 
   pushd "${repo_dir}" >/dev/null
   bash "${repo_dir}/installer_scripts/ubuntu_bare_minimum.bash"
