e12eafe536689e6cc03cf6188d071e68866acd9b
Wojciech Kaluza <wkaluza@protonmail.com>
2023-01-08 21:10:04 +0100

    Create kubernetes shell helper import

---
+++ shell_script_imports/internal/k8s_helpers.bash
@@ -0,0 +1,30 @@
+set -euo pipefail
+if command -v shopt &>/dev/null; then
+  shopt -s inherit_errexit
+fi
+
+function _list_yaml_files
+{
+  local dir="$1"
+
+  find "${dir}" -iname '*.yaml' -type f
+}
+
+function _print_resource
+{
+  local name="$1"
+
+  echo "  - $(basename "${name}")"
+}
+
+function generate_kustomization_yaml_for_directory
+{
+  local dir="$1"
+
+  cat <<EOF >"${dir}/kustomization.yaml"
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+$(_list_yaml_files "${dir}" | sort | for_each _print_resource)
+EOF
+}
