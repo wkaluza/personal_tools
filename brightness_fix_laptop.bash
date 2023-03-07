1bef136b59526da674f85c009f74ccf00cb98661
Wojciech Kaluza <wkaluza@protonmail.com>
2023-03-07 10:35:57 +0100

    Clean up old brightness script instances

--- brightness_fix_laptop.bash
+++ brightness_fix_laptop.bash
@@ -49,8 +49,70 @@ function handle_brightness_changes
   disown
 }
 
+function _list_script_instances
+{
+  local this_script_path="$1"
+
+  pgrep -ax bash |
+    no_fail grep -E "^.+ bash ${this_script_path}$" |
+    cut -d' ' -f1 |
+    sort -n |
+    uniq
+}
+
+function kill_process
+{
+  local pid="$1"
+
+  kill \
+    -s "TERM" \
+    "${pid}"
+}
+
+function is_not_child_process
+{
+  local parent="$1"
+  local maybe_child="$2"
+
+  if pgrep -P "${parent}" |
+    grep -E "^${maybe_child}$" &>/dev/null; then
+
+    return 1
+  fi
+
+  echo "${maybe_child}"
+}
+
+function pid_still_exists
+{
+  local pid="$1"
+
+  if ! ps -p "${pid}" &>/dev/null; then
+    return 1
+  fi
+
+  echo "${pid}"
+}
+
+function kill_old_instances
+{
+  local this_script_path="$1"
+
+  local current_process="$$"
+
+  local instances
+  instances="$(_list_script_instances "${this_script_path}" |
+    for_each filter is_not_child_process "${current_process}" |
+    grep -Ev "^${current_process}$")"
+
+  echo "${instances}" |
+    for_each filter pid_still_exists |
+    for_each kill_process
+}
+
 function main
 {
+  kill_old_instances "$0"
   handle_brightness_changes &>/dev/null
 
   log_info "Success $(basename "$0")"
